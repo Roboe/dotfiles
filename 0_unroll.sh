2245770bc200a4e3579cba516fdc70cdc021dee9
Roberto MF <git@virgulilla.com>
2019-08-17 02:34:34 +0200

    Change dotfiles cloning strategy

---
+++ 0_unroll.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+DOTFILES_D=.dotfiles
+
+cd ~
+
+
+# Unroll files
+for SOURCE in $(find $DOTFILES_D -maxdepth 1 -type f -name ".*")
+do
+  DESTINATION=$(basename $SOURCE)
+  [[ -f $SOURCE ]] && ln --symbolic --interactive $SOURCE $DESTINATION
+done
+
+
+# Unroll folders
+function unroll_folder {
+  local SOURCE=$1
+  local DESTINATION=$2
+  [[ -d $DESTINATION ]] && echo "Merging $DESTINATION folder..." && cp --recursive $DESTINATION/* $SOURCE
+  rm -rf $DESTINATION
+  ln --symbolic $SOURCE $DESTINATION
+}
+
+unroll_folder $DOTFILES_D/.bashrc.d .bashrc.d
+unroll_folder ~/$DOTFILES_D/.local/share/nautilus/scripts .local/share/nautilus/scripts
+
+
+unset DOTFILES_D
---
+++ README.md
@@ -0,0 +1,20 @@
+dotfiles
+===
+
+My personal dotfiles for my GNU machines.
+
+Installing
+---
+
+It's suposed to be cloned in a `.dotfiles` folder right in a user's home:
+
+```
+git clone https://gitlab.com/Roboe/dotfiles.git ~/.dotfiles
+```
+
+Then, execute the `0_unroll.sh` script to create symbolic links:
+
+```
+cd ~/.dotfiles
+./0_unroll.sh
+```
