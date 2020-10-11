Roboe/dotfiles

commit 231a60979d9dd4c43d27aa025530eeb10f0cdbd4

Replace 'which' with the POSIX-compatible 'command -v'

.bashrc.d/3-dev-environment/python.bash
@@ -1,6 +1,6 @@
 # Python 3 by default if available
 ## To use Python 2, use 'python2' instead
-[[ $(which python3) ]] && alias python='python3'
+[[ $(check_command python3) ]] && alias python='python3'
 
 
 # Python VirtualEnvWrapper setup

.bashrc.d/4-aliases-functions/apt-why.bash
@@ -1,2 +1,2 @@
 ## Alias to query why a package was autoinstalled
-[[ $(which apt) ]] && alias apt-why='apt rdepends --no-{suggests,conflicts,breaks,replaces,enhances} --installed --recurse'
+[[ $(check_command apt) ]] && alias apt-why='apt rdepends --no-{suggests,conflicts,breaks,replaces,enhances} --installed --recurse'

.bashrc.d/4-aliases-functions/install-with-gnome-software.bash
@@ -1,4 +1,4 @@
 ## Install local package with Gnome Software
-[[ $(which gnome-software) ]] && function software-install {
+[[ $(check_command gnome-software) ]] && function software-install {
   gnome-software --local-filename="$1"
 }

.bashrc.d/4-aliases-functions/static-servers.bash
@@ -1,18 +1,18 @@
 ## HTTP server. From this superuseful Gist: https://gist.github.com/willurd/5720255
-[[ $(which python2) ]] && alias serve2='echo "Serving on http://localhost:8000/" && python2 -m SimpleHTTPServer 8000'
-[[ $(which python3) ]] && alias serve3='echo "Serving on http://localhost:8000/" && python3 -m http.server 8000'
+[[ $(check_command python2) ]] && alias serve2='echo "Serving on http://localhost:8000/" && python2 -m SimpleHTTPServer 8000'
+[[ $(check_command python3) ]] && alias serve3='echo "Serving on http://localhost:8000/" && python3 -m http.server 8000'
 
 
 ## FTP server. Prerequisite: 'python-pyftpdlib' or 'python3-pyftpdlib' package
-[[ $(which python2) ]] && alias ftpserve2='echo "Serving on http://localhost:2121/" && python2 -m pyftpdlib'
-[[ $(which python3) ]] && alias ftpserve3='echo "Serving on http://localhost:2121/" && python3 -m pyftpdlib'
+[[ $(check_command python2) ]] && alias ftpserve2='echo "Serving on http://localhost:2121/" && python2 -m pyftpdlib'
+[[ $(check_command python3) ]] && alias ftpserve3='echo "Serving on http://localhost:2121/" && python3 -m pyftpdlib'
 
 
-if [[ $(which python2) ]]
+if [[ $(check_command python2) ]]
 then # Prefer python2 server
   alias serve='serve2'
   alias ftpserve='ftpserve2'
-elif [[ $(which python3) ]]
+elif [[ $(check_command python3) ]]
 then # Fallback to python3 server
   alias serve='serve3'
   alias ftpserve='ftpserve3'

.bashrc.d/4-aliases-functions/system-update.bash
@@ -3,25 +3,25 @@ alias upper='up --per'
 function up {
   wait-for-network
 
-  if [[ $(which apt) ]]
+  if [[ $(check_command apt) ]]
   then
     echolorize --title "$(emoji "package") APT"
     apt-up "$@"
   fi
 
-  if [[ $(which dnf) ]]
+  if [[ $(check_command dnf) ]]
   then
     echolorize --title "$(emoji "package") DNF"
     dnf-up "$@"
   fi
 
-  if [[ $(which pacman) ]]
+  if [[ $(check_command pacman) ]]
   then
     echolorize --title "$(emoji "package") PACMAN"
     pacman-up "$@"
   fi
 
-  if [[ $(which flatpak) ]]
+  if [[ $(check_command flatpak) ]]
   then
     echolorize --title "$(emoji "package") FLATPAK"
     flatpak-up "$@"
@@ -69,7 +69,7 @@ function dnf-up {
 
 ## Arch/Parabola
 function pacman-up {
-  if [[ $(which yaourt) ]]
+  if [[ $(check_command yaourt) ]]
   then
     echolorize "$(emoji "community") SYNC UPDATES FROM AUR"
     yaourt --sync --refresh --upgrades --aur

.bashrc.d/5-graphical-output/screenfetch.bash
@@ -1,2 +1,2 @@
 # Show distro and screen info
-[[ $(which screenfetch) ]] && screenfetch
+[[ $(check_command screenfetch) ]] && screenfetch

.bashrc.d/1-terminal-setup/check_command.bash
@@ -1,0 +1,5 @@
+# POSIX way to check if a command is in PATH and it's executable
+# https://stackoverflow.com/q/592620
+function check_command {
+  test -x "$(command -v $1)"
+}

commit c2f2bc1b3252396e35b7398971dd7219c6255b66

Fix redundant tests in conditional clauses that where failing since 231a6097

.bashrc.d/3-dev-environment/python.bash
@@ -1,6 +1,6 @@
 # Python 3 by default if available
 ## To use Python 2, use 'python2' instead
-[[ $(check_command python3) ]] && alias python='python3'
+check_command python3 && alias python='python3'
 
 
 # Python VirtualEnvWrapper setup

.bashrc.d/4-aliases-functions/apt-why.bash
@@ -1,2 +1,2 @@
 ## Alias to query why a package was autoinstalled
-[[ $(check_command apt) ]] && alias apt-why='apt rdepends --no-{suggests,conflicts,breaks,replaces,enhances} --installed --recurse'
+check_command apt && alias apt-why='apt rdepends --no-{suggests,conflicts,breaks,replaces,enhances} --installed --recurse'

.bashrc.d/4-aliases-functions/install-with-gnome-software.bash
@@ -1,4 +1,4 @@
 ## Install local package with Gnome Software
-[[ $(check_command gnome-software) ]] && function software-install {
+check_command gnome-software && function software-install {
   gnome-software --local-filename="$1"
 }

.bashrc.d/4-aliases-functions/static-servers.bash
@@ -1,18 +1,18 @@
 ## HTTP server. From this superuseful Gist: https://gist.github.com/willurd/5720255
-[[ $(check_command python2) ]] && alias serve2='echo "Serving on http://localhost:8000/" && python2 -m SimpleHTTPServer 8000'
-[[ $(check_command python3) ]] && alias serve3='echo "Serving on http://localhost:8000/" && python3 -m http.server 8000'
+check_command python2 && alias serve2='echo "Serving on http://localhost:8000/" && python2 -m SimpleHTTPServer 8000'
+check_command python3 && alias serve3='echo "Serving on http://localhost:8000/" && python3 -m http.server 8000'
 
 
 ## FTP server. Prerequisite: 'python-pyftpdlib' or 'python3-pyftpdlib' package
-[[ $(check_command python2) ]] && alias ftpserve2='echo "Serving on http://localhost:2121/" && python2 -m pyftpdlib'
-[[ $(check_command python3) ]] && alias ftpserve3='echo "Serving on http://localhost:2121/" && python3 -m pyftpdlib'
+check_command python2 && alias ftpserve2='echo "Serving on http://localhost:2121/" && python2 -m pyftpdlib'
+check_command python3 && alias ftpserve3='echo "Serving on http://localhost:2121/" && python3 -m pyftpdlib'
 
 
-if [[ $(check_command python2) ]]
+if check_command python2
 then # Prefer python2 server
   alias serve='serve2'
   alias ftpserve='ftpserve2'
-elif [[ $(check_command python3) ]]
+elif check_command python3
 then # Fallback to python3 server
   alias serve='serve3'
   alias ftpserve='ftpserve3'

.bashrc.d/4-aliases-functions/system-update.bash
@@ -3,25 +3,25 @@ alias upper='up --per'
 function up {
   wait-for-network
 
-  if [[ $(check_command apt) ]]
+  if check_command apt
   then
     echolorize --title "$(emoji "package") APT"
     apt-up "$@"
   fi
 
-  if [[ $(check_command dnf) ]]
+  if check_command dnf
   then
     echolorize --title "$(emoji "package") DNF"
     dnf-up "$@"
   fi
 
-  if [[ $(check_command pacman) ]]
+  if check_command pacman
   then
     echolorize --title "$(emoji "package") PACMAN"
     pacman-up "$@"
   fi
 
-  if [[ $(check_command flatpak) ]]
+  if check_command flatpak
   then
     echolorize --title "$(emoji "package") FLATPAK"
     flatpak-up "$@"
@@ -72,7 +72,7 @@ function dnf-up {
 
 ## Arch/Parabola
 function pacman-up {
-  if [[ $(check_command yaourt) ]]
+  if check_command yaourt
   then
     echolorize "$(emoji "community") SYNC UPDATES FROM AUR"
     yaourt --sync --refresh --upgrades --aur

.bashrc.d/5-graphical-output/screenfetch.bash
@@ -1,2 +1,2 @@
 # Show distro and screen info
-[[ $(check_command screenfetch) ]] && screenfetch
+check_command screenfetch && screenfetch